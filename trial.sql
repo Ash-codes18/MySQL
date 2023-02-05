2a472a58625016ca48066fe74e34801c27e11c37
Ashmit Mehta <amehta.ashmit2@gmail.com>
2023-02-05 16:12:13 +0530

    anime

--- trial.sql
+++ trial.sql
@@ -1,2 +1,12 @@
-use int306;
-desc users;
+use anime;
+create table Anime(
+    S_no int(10),
+    Title char(50),
+    Rating float(3,2),
+    genre char(50),
+    Released_in int(4) 
+    );
+insert into Anime
+values (1,'Death Note',9.0,'Thriller',2003);
+use anime;
+select * from anime;
